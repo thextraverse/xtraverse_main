1d36e4125c797598163f5435732948025a62d7d0
isaiahverse <127069420+isaiahverse@users.noreply.github.com>
2023-03-19 16:56:19 -0400

    Update and rename components/dashboard/luanch/DomainIntergration.js to components/dashboard/launch/DomainIntergration.js

--- components/dashboard/launch/DomainIntergration.js
+++ components/dashboard/launch/DomainIntergration.js
@@ -1,6 +1,28 @@
 import styled from "@emotion/styled";
 import { Box, Button, Grid } from "@mui/material";
 import { Form } from "../../styles/homepage.styled";
+import { registerDomain, addDomainToFirebase, deployWebsite } from './GoDaddy';
+
+function WebsiteForm() {
+  const [domainName, setDomainName] = useState('');
+  const [projectId, setProjectId] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    // Register domain with GoDaddy
+    const authorizationToken = await getAuthorizationToken();
+    const registeredDomain = await registerDomain(authorizationToken, domainName);
+
+    // Add domain to Firebase
+    const firebaseAccessToken = process.env.FIREBASE_ACCESS_TOKEN;
+    const addedDomain = await addDomainToFirebase(projectId, registeredDomain, firebaseAccessToken);
+
+    // Deploy website to Firebase Hosting
+    const websiteId = 'your-website-id'; // Replace with your website ID
+    await deployWebsite(projectId, websiteId, addedDomain);
+  };
+
 
 const FBform = styled.div`
   width: 500px;
